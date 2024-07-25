4356a0d4887e9ba37a448ab1449ae0173138ffc5
Buntcz <testtigeryt@gmail.com>
2024-07-25 22:22:36 +0300

    Did the styling.

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,9 +1,9 @@
 function Product({product}) {
     return (
         <div className="productCard">
-            <p>{product.productName}</p>
-            <p>{product.productPrice}</p>
             <img src={product.productImage} />
+            <p>{product.productName}</p>
+            <p>{product.productPrice}$</p>
             <button>Add to cart</button>
         </div>
     )
--- src/pages/shop/products.js
+++ src/pages/shop/products.js
@@ -2,49 +2,49 @@ const products = [
     {
         id: 1,
         productName: "Iphone",
-        productImage: "./assets/products/1.png",
+        productImage: "./products/1.png",
         productPrice: 999
     },
     {
         id: 2,
         productName: "Macbook",
-        productImage: "../assets/products/2.png",
+        productImage: "./products/2.png",
         productPrice: 999
     },
     {
         id: 3,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/3.png",
         productPrice: 999
     },
     {
         id: 4,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/4.png",
         productPrice: 999
     },
     {
         id: 5,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/5.png",
         productPrice: 999
     },
     {
         id: 6,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/6.webp",
         productPrice: 999
     },
     {
         id: 7,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/7.webp",
         productPrice: 999
     },
     {
         id: 8,
         productName: "Iphone",
-        productImage: "../assets/products/1.png",
+        productImage: "./products/8.webp",
         productPrice: 999
     }
 ]
